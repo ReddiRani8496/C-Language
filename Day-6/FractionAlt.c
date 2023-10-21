40c501910306d75368c648b367a4224c0d6c3fdc
reddirani8496 <reddiranibattuluri@gmail.com>
2023-10-21 21:08:59 +0530

    Day-6 programs done

--- Day-6/FractionAlt.c
+++ Day-6/FractionAlt.c
@@ -3,11 +3,15 @@ int main() {
     int n=5;
     int numerator=1;
     int i;
-    for(i=1;i<n;i++){
+    for(i=1;i<n;i++) {
+
         if(i%2==0)
             printf("%d/%d + ",numerator,i);
+
         else
             printf("%d/%d - ",numerator,i);
+
     }
+    
     printf("%d/%d",numerator,i);
 }
