8de6959d8a6dac4d4de026102f331cfdc001646c
reddirani8496 <reddiranibattuluri@gmail.com>
2023-12-20 18:51:23 +0530

    done

--- Day-15/nestedstructures.c
+++ Day-15/nestedstructures.c
@@ -6,12 +6,12 @@ typedef struct Car
     float price;
 
     char color[20];
-    
+
 }car;
 
 typedef struct Audi
 {
-    car c1;
+    car c1,*ptr1;
 
    float mileage; 
 
@@ -19,17 +19,21 @@ typedef struct Audi
 
 int main() {
 
+    car c1;
+
     audi a1,*ptr;
 
     ptr = &a1;
 
-    ptr->c1.price=50000.0;
+    ptr->ptr1=&c1;
+
+    ptr->ptr1->price=50000.0;
 
     ptr-> mileage = 14;
 
     strcpy(ptr->c1.color,"Black");
 
-    printf("%f\n",(ptr->c1.price));
+    printf("%f\n",(ptr->ptr1->price));
     
     printf("%f\n",ptr->mileage);
 
